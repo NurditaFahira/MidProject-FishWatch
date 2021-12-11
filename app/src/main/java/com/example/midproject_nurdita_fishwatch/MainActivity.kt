db6d12232f3acdc001503ede68ac28f379b8ceea
Nurdita Fahira <dhitafahira22@gmail.com>
2021-12-12 02:09:48 +0800

    Implementing viewModel

--- app/src/main/java/com/example/midproject_nurdita_fishwatch/MainActivity.kt
+++ app/src/main/java/com/example/midproject_nurdita_fishwatch/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.midproject_nurdita_fishwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.midproject_nurdita_fishwatch.UI.Home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val fragment: Fragment = HomeFragment()
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+
+        if(savedInstanceState == null){
+            fragmentTransaction.replace(R.id.container_fragment, fragment)
+            fragmentTransaction.commitNow()
+        }
+
+        //fragmentTransaction.addToBackStack(null)
+
+       // fragmentManager.executePendingTransactions()
+
     }
 }
---
+++ app/src/main/java/com/example/midproject_nurdita_fishwatch/UI/Home/HomeViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.midproject_nurdita_fishwatch.UI.Home
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel(){
+
+    var number = 0
+    init{
+        Log.d("HomeViewModel", "Created")
+    }
+
+
+}
